845c35243dc6b820c057ab72af485c87bacce1ee
simscr <simscr90@gmail.com>
2022-12-09 14:12:31 -0600

    more aoc

--- r_Rmd/aoc_2022.R
+++ r_Rmd/aoc_2022.R
@@ -6,6 +6,7 @@ pacman::p_load(tidyverse, rio, here, janitor, ggpubr, rstatix)
 conflicted::conflict_prefer("select", "dplyr")
 conflicted::conflict_prefer("filter", "dplyr")
 conflicted::conflict_prefer("clean_names", "janitor")
+conflicted::conflict_prefer("lag", "dplyr")
 
 theme_set(theme_bw(base_size = 15) + 
             theme(legend.position = "bottom", 
@@ -266,3 +267,237 @@ purrr::map_chr(stacks, tail, 1) |> paste(collapse = "")
 
 # d5.2 = GCFGLDNJZ
 
+
+# Day 6 -------------------------------------------------------------------
+
+tictoc::tic()
+d6.1 <- readLines(here("data", "aoc", "2022", "d6.txt")) %>% 
+  strsplit(split = "") %>% 
+  unlist()
+
+d6.1 %>% 
+  as_tibble() %>% 
+  mutate(row_number = row_number(),
+         letter_set = paste0(lag(value, 3), ", ", lag(value, 2), ", ", lag(value, 1), ", ", value)) %>% 
+  rowwise() %>% 
+  mutate(dupe_total = sum(duplicated(unlist(strsplit(letter_set, split = ", "))))) %>% 
+  filter(dupe_total == 0) # row_number is answer
+
+# tictoc::toc()
+
+# d6.1 = 1855
+
+# for (n_letter in seq_along(d6.1)) {
+#   l1 <- ifelse(n_letter - 3 <= 0, NA_character_, d6.1[n_letter - 3])
+#   l2 <- ifelse(n_letter - 2 <= 0, NA_character_, d6.1[n_letter - 2])
+#   l3 <- ifelse(n_letter - 1 <= 0, NA_character_, d6.1[n_letter - 1])
+#   l4 <- ifelse(n_letter <= 0, NA_character_, d6.1[n_letter])
+# 
+#   lets <- paste0(c(l1, l2, l3, l4), collapse = ", ")
+# 
+#   value <- sum(duplicated(lets))
+#   
+#   # print(paste0(value, ":", lets))
+#   
+#   # print(list(value, lets))
+#   
+#   # print(tibble(
+#   #   letter_set = lets, 
+#   #   rep_value = value
+#   # ))
+# 
+# }
+
+
+# There's probably a better way to do this, but this worked :)
+d6.1 %>% 
+  as_tibble() %>% 
+  mutate(row_number = row_number(),
+         letter_set = paste0(lag(value, 13), ", ", lag(value, 12), ", ", lag(value, 11), ", ",
+                             lag(value, 10), ", ", lag(value, 9), ", ", lag(value, 8), ", ",
+                             lag(value, 7), ", ", lag(value, 6), ", ", lag(value, 5), ", ",
+                             lag(value, 4), ", ", lag(value, 3), ", ", lag(value, 2), ", ",
+                             lag(value, 1), ", ", value)) %>% 
+  rowwise() %>% 
+  mutate(dupe_total = sum(duplicated(unlist(strsplit(letter_set, split = ", "))))) %>% 
+  filter(dupe_total == 0) # row_number is answer
+
+tictoc::toc()
+# d6.2 = 3256
+
+
+# Day 7 -------------------------------------------------------------------
+
+d7 <- readLines(here("data", "aoc", "2022", "d7.txt"))
+
+d7_work <- d7 %>% as_tibble() %>% 
+  mutate(id = row_number(), 
+         size = parse_number(value), 
+         level_0 = case_when(value == "$ cd /" ~ "root", 
+                             TRUE ~ NA_character_),
+         level_1 = case_when(str_detect(value, "cd [:lower:]") ~ value,
+                             TRUE ~ NA_character_), 
+         move_in = case_when(str_detect(value, "cd [:lower:]") & !is.na(level_1) ~ TRUE,
+                             TRUE ~ FALSE))
+
+
+
+sum(d7_work$size, na.rm = TRUE)
+
+input <- d7
+
+# EH
+disk <- list()
+i <- 2
+path <- c(NULL)
+
+while (i <= length(input)) {
+  if (input[i] == "$ ls") {
+    repeat {
+      i <- i + 1
+      if (i > length(input)) break
+      if (str_detect(input[i], "^\\$")) break
+      if (str_detect(input[i], "^dir")) {
+        dir_name <- str_remove(input[i], "dir ")
+        disk[[c(path, dir_name)]] <- list()
+      } else {
+        file_name <- str_remove(input[i], "\\d+ ")
+        file_value <- str_extract(input[i], "\\d+") |> as.numeric()
+        disk[[c(path, file_name)]] <- file_value
+      }
+    }
+  } else if (str_detect(input[i], "^\\$ cd \\.\\.")) {
+    path <- path[-length(path)]
+    i <- i + 1
+  } else if (str_detect(input[i], "^\\$ cd")) {
+    next_dir <- str_remove(input[i], "^\\$ cd ")
+    path <- c(path, next_dir)
+    i <- i + 1
+  }
+}
+
+dir_sums <- list()
+
+lists <- map_lgl(disk, is.list)
+unused_folders <- as.list(names(disk)[lists])
+
+repeat {
+  if (length(unused_folders) == 0) break
+  path <- unused_folders[[1]]
+  lists <- map_lgl(disk[[path]], is.list)
+  if (any(lists)) {
+    new_lists <- names(lists)[lists]
+    for (new_list in new_lists) {
+      unused_folders <- c(list(c(path, new_list)), unused_folders)
+    }
+  } else {
+    dir_sum <- sum(unlist(disk[[path]]))
+    dir_sums[paste(path, collapse = "_")] <- dir_sum
+    disk[[path]] <- dir_sum
+    unused_folders[[1]] <- NULL
+  }
+}
+
+dir_sums$total <- sum(unlist(disk))
+dir_sums <- unlist(dir_sums)
+
+dir_sums[dir_sums < 100000] |>
+  sum()
+
+
+# d7.1 = 1118405
+
+input <- d7
+
+disk <- list()
+i <- 2
+path <- c(NULL)
+
+while (i <= length(input)) {
+  if (input[i] == "$ ls") {
+    repeat {
+      i <- i + 1
+      if (i > length(input)) break
+      if (str_detect(input[i], "^\\$")) break
+      if (str_detect(input[i], "^dir")) {
+        dir_name <- str_remove(input[i], "dir ")
+        disk[[c(path, dir_name)]] <- list()
+      } else {
+        file_name <- str_remove(input[i], "\\d+ ")
+        file_value <- str_extract(input[i], "\\d+") |> as.numeric()
+        disk[[c(path, file_name)]] <- file_value
+      }
+    }
+  } else if (str_detect(input[i], "^\\$ cd \\.\\.")) {
+    path <- path[-length(path)]
+    i <- i + 1
+  } else if (str_detect(input[i], "^\\$ cd")) {
+    next_dir <- str_remove(input[i], "^\\$ cd ")
+    path <- c(path, next_dir)
+    i <- i + 1
+  }
+}
+
+dir_sums <- list()
+
+lists <- map_lgl(disk, is.list)
+unused_folders <- as.list(names(disk)[lists])
+
+repeat {
+  if (length(unused_folders) == 0) break
+  path <- unused_folders[[1]]
+  lists <- map_lgl(disk[[path]], is.list)
+  if (any(lists)) {
+    new_lists <- names(lists)[lists]
+    for (new_list in new_lists) {
+      unused_folders <- c(list(c(path, new_list)), unused_folders)
+    }
+  } else {
+    dir_sum <- sum(unlist(disk[[path]]))
+    dir_sums[paste(path, collapse = "_")] <- dir_sum
+    disk[[path]] <- dir_sum
+    unused_folders[[1]] <- NULL
+  }
+}
+
+dir_sums$total <- sum(unlist(disk))
+dir_sums <- unlist(dir_sums)
+
+dir_sums[dir_sums >= (30000000 - (70000000 - dir_sums["total"]))] |>
+  sort() |>
+  head(1)
+
+# d7.2 = 12545514 
+
+
+# Day 8 -------------------------------------------------------------------
+
+
+# Functions from David Robinson
+parse_txt <- function(s, ...) {
+  readr::parse_guess(s, locale = readr::locale("en", grouping_mark = ""))
+}
+
+grid_tidy <- function(d, var, sep = "", parse = TRUE) {
+  ret <- d %>%
+    mutate(row = row_number()) %>%
+    mutate(value = stringr::str_split({{ var }}, sep)) %>%
+    select(-{{ var }}) %>%
+    tidyr::unnest(value) %>%
+    group_by(row) %>%
+    mutate(col = row_number()) %>%
+    ungroup()
+  
+  if (parse) {
+    ret <- ret %>%
+      mutate(value = parse_txt(value))
+  }
+  
+  ret
+}
+
+d8 <- read_tsv(here("data", "aoc", "2022", "d8.txt"), col_names = FALSE)
+
+d8 %>% 
+  grid_tidy(X1, parse = TRUE)
+  
